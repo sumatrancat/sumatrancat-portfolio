408b66c215f800c989a1509c995592b0ba6cfa82
Sumatrancat <sumatrancat@gmail.com>
2023-09-29 15:06:29 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Sumatrancat's Portfolio Website
+
+This website uses the following tools to achieve its features.
+1. [Rive](https://rive.app) : Web-based tool to animate interactive vector graphics
+2. [GSAP](https://greensock.com) : JS plugin to animate pretty much anything you want on the web
+3. [Parcel](https://parceljs.com) : Code bundler
+4. [Pug](https://pugjs.com) : HTML Static Generator
+5. [SASS](https://sass-lang.com/) : CSS Pre-Processor (Yes, I still use this)
+
+## How to run
+1. Fork this repo
+2. Install with `npm install`
+3. Run with `npm start`
+
+## Credits
+I designed and coded the website by myself. Artwork is heavily inspired by [Deekay Kwon](https://deekaykwon.com/)
---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+const rive = require('@rive-app/canvas')
+const riveFile = new URL('../rive/sumatrancat12.riv', import.meta.url)
+
+const artboardSC = loadArtboard('Sumatrancat', '.ab-sc')
+const artboardDesk = loadArtboard('Desk', '.ab-desk')
+const artboardMusic = loadArtboard('Music', '.ab-music')
+const artboardSocialResume = loadArtboard('SocialResume', '.ab-social-resume')
+
+function loadArtboard(name, wrapper) {
+  if(riveFile) {
+    const artboard = new rive.Rive({
+      src: riveFile,
+      artboard: name,
+      stateMachines: 'SM',
+      canvas: document.querySelector(wrapper),
+      onLoad: e => {
+        artboard.resizeDrawingSurfaceToCanvas()
+        artboard.play()
+      }
+    })
+    return artboard
+  }
+}
