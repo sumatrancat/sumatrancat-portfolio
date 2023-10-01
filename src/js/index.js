3a2a9d8141e5256e8d29acf9f790050935363ae3
Sumatrancat <sumatrancat@gmail.com>
2023-10-02 00:23:15 +0700

    added mask/clip on home title using svg

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
+import gsap from 'gsap'
+import { SplitText } from 'gsap/all'
+
 const rive = require('@rive-app/canvas')
 const riveFile = new URL('../rive/sumatrancat.riv', import.meta.url)
 
@@ -21,3 +24,20 @@ function loadArtboard(name, wrapper) {
     return artboard
   }
 }
+
+// gsap.registerPlugin(SplitText)
+
+// const animatedTitle = new SplitText('#home-title', {
+//   type: 'chars'
+// })
+// const tl = gsap.timeline({
+//   repeat: -1,
+//   yoyo: true
+// })
+
+// tl
+//   .to('#home-title > div', {
+//     y: -15,
+//     stagger: .009,
+//     ease: 'power2.out'
+//   })
